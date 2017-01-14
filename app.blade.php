7016746437cd1b59f3b637600a30fbc748272703
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2017-01-14 21:00:26 +0100

    updates

--- app.blade.php
+++ app.blade.php
@@ -35,7 +35,7 @@
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul class="nav navbar-nav">
-                <li><a href="#">Blog</a></li>
+                <li><a href="#">Posts</a></li>
                 <li><a href="#">Comments</a></li>
             </ul>
             <form class="navbar-form navbar-left">
